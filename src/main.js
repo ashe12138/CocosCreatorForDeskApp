bf6d157ceadd2c22911d059f91071df75dbc8efb
jazzg62 <30382988+ashe12138@users.noreply.github.com>
2020-05-07 18:22:21 +0800

    hide the menu bar

--- src/main.js
+++ src/main.js
@@ -2,6 +2,10 @@ global.Editor = require('./editor.js');
 const Window = require('./window.js');
 const Server = require('./server.js');
 const {app} = require('electron');
+
+const {Menu} = app;
+Menu.setApplicationMenu(null);
+
 app.on('ready', function () {
     Server.initServer(function (url) {
         Window.createMainWindow(url);
